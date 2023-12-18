07cdd8e9d2fe0e16e10ca246aa82fdf6c78a5a58
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-12-18 08:45:15 -0300

    add banner style in html

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MoviedbService } from '../../services/moviedb.service';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
@@ -13,9 +15,11 @@ export class BannerComponent {
   @Input() movieTitle: string = '';
   @Input() movieYear: number = 0;
   @Input() moviePlot: number = 0;
-  @Input() moviePoster: string = ''; //
-  @Input() movieRuntime: string = ''; //
+  @Input() moviePoster: string = '';
+  @Input() movieRuntime: string = '';
   @Input() movieimdbRating: string = '';
+  @Input() movieBgPoster:string = '';
+
 
   constructor(private moviedbService: MoviedbService) {}
 
@@ -23,10 +27,16 @@ export class BannerComponent {
     this.loadMovieDetails();
   }
 
+  get backgroundStyle() {
+    return {
+      'background': `linear-gradient(to left, rgba(245, 246, 252, 0.025), rgba(10, 10, 10, 0.847)), url(${this.movieBgPoster}) center/cover no-repeat`
+    };
+  }
+
   private loadMovieDetails() {
     this.moviedbService.getMovieByTitle(this.movieTitle).subscribe(
       (data) => {
-        console.log('Detalhes do Filme:', data);
+        //console.log('Detalhes do Filme:', data);
         this.movieYear = data.Year;
         this.moviePlot = data.Plot;
         this.moviePoster = data.Poster;
@@ -34,6 +44,7 @@ export class BannerComponent {
         const { horas, minutos: minutosRestantes } = this.converterMinutosParaHoras(minutosConvertidos);
         this.movieRuntime = `${horas} h ${minutosRestantes} min`;
         this.movieimdbRating = data.imdbRating;
+        /* this.movieBgPoster = `linear-gradient(to left, rgba(245, 246, 252, 0.025), rgba(10, 10, 10, 0.847)), center/cover no-repeat`; */
       },
       (error) => {
         console.error('Erro ao obter detalhes do filme:', error);
@@ -51,7 +62,6 @@ export class BannerComponent {
     this.moviedbService.getMovieByTitle(this.movieTitle).subscribe(
       (data) => {
         console.log('Detalhes do Filme:', data);
-        // Aqui você pode manipular os dados conforme necessário
       },
       (error) => {
         console.error('Erro ao obter detalhes do filme:', error);
