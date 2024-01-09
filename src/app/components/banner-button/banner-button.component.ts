8011e0f54e5466f57966d6da287bae9a641b5cb7
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-01-09 09:59:56 -0300

    fixed button banner router

--- src/app/components/banner-button/banner-button.component.ts
+++ src/app/components/banner-button/banner-button.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-banner-button',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './banner-button.component.html',
   styleUrl: './banner-button.component.css'
 })
