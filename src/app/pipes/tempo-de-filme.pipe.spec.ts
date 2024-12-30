e3473da70edb7ea64947956bae9c351e57fc3c7f
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-12-29 22:49:02 -0300

    fix tempo-de-filme

--- src/app/pipes/tempo-de-filme.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TempoDeFilmePipe } from './tempo-de-filme.pipe';
-
-describe('TempoDeFilmePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TempoDeFilmePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/pipes/tempo-de-filme.pipe.ts
+++ src/app/pipes/tempo-de-filme.pipe.ts
@@ -7,6 +7,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TempoDeFilmePipe implements PipeTransform {
 
   transform(minutos: string): string {
+    if (!minutos || isNaN(parseInt(minutos.match(/\d+/)?.[0] || '', 10))) {
+      return 'N/A';
+    }
+
     const minutosComoNumero: number = parseInt(minutos.match(/\d+/)?.[0] || '0', 10);
 
     const horas: number = Math.floor(minutosComoNumero / 60);
@@ -15,9 +19,9 @@ export class TempoDeFilmePipe implements PipeTransform {
     const horasFormatadas = horas > 0 ? `${horas}h` : '';
     const minutosFormatados = minutosRestantes > 0 ? `${minutosRestantes}min` : '';
 
-    const resposta = `${horasFormatadas} ${minutosFormatados}`;
+    const resposta = `${horasFormatadas} ${minutosFormatados}`.trim();
 
-    return resposta;
+    return resposta || '0min';
   }
 
 }
